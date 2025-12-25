47fa304a07327a812175a5002c5ca0bfa2223a31
abhishekabhi123 <vadakkemadamabhishek@gmail.com>
2025-12-25 14:47:13 +0530

    added tabs

---
+++ app/(tabs)/settings.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const settings = () => {
+  return (
+    <View>
+      <Text>This is settings page</Text>
+    </View>
+  );
+};
+
+export default settings;
